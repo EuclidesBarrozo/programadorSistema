b1115d6d1d8ab09329244efc53ff5406d1d8cf53
EuclidesBarrozo <euclidesbarrozo@hotmail.com>
2024-04-02 07:42:43 -0300

    aula0204

---
+++ Console/CalculadoraPOO1/CalculadoraPOO1/Calculadora.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPOO1
+{
+    public class Calculadora
+    {
+        //atributos ou propriedades
+        public double n1;
+        public double n2;
+        public double resultado;
+        public string operador;
+
+        //métodos
+        public double somar()
+        {
+            this.resultado = this.n1 + this.n2;
+            return this.resultado;
+        }
+
+        public double subtrair()
+        {
+            this.resultado = this.n1 - this.n2;
+            return this.resultado;
+        }
+
+        public double multiplicar()
+        {
+            this.resultado = this.n1 * this.n2;
+            return this.resultado;
+        }
+
+        public double dividir()
+        {
+            this.resultado = this.n1 / this.n2;
+            return this.resultado;
+        }
+    }
+}
