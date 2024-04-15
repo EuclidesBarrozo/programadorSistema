e9a2fa80011324cf7c245c10e7f986640656705f
EuclidesBarrozo <euclidesbarrozo@hotmail.com>
2024-04-15 11:17:40 -0300

    aula150424

--- Forms/BancoDeTalentos/BancoDeTalentos/Banco.cs
+++ Forms/BancoDeTalentos/BancoDeTalentos/Banco.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data;
 using System.Data.SQLite;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
 
 namespace BancoDeTalentos
 {
@@ -13,15 +15,15 @@ namespace BancoDeTalentos
         //atributo
         private static SQLiteConnection conexao;
         
-        //métod ConexaoBanco
+        //método ConexaoBanco retorna uma conexão da classe SQLiteConnection
         private static SQLiteConnection ConexaoBanco()
         {
             //Criar um conexao do tipo SQLiteConnect
             //linkado ao arquivo banco.db
             conexao = new SQLiteConnection("Data Source = ..\\..\\..\\banco\\banco.db");
-            //abrindo a conexão
+            //abrindo a conexão utilizando o método Open()
             conexao.Open();
-            //retorna para o arquivo que o chamou a conexao
+            //retorna para o arquivo/método que chamou a conexao
             return conexao;
         }
 
@@ -48,12 +50,12 @@ namespace BancoDeTalentos
         }
 
         //método consulta que recebe uma string com o comando sql
-        //e retornar uma tabela de dados
+        //e retornar uma tabela de dados(DataTable)
         public static DataTable consulta(string sql)
         {
-            //adaptador de dados inciado com null
+            //adaptador de dados inciado com null (formato do SQL)
             SQLiteDataAdapter da = null;
-            //objeto da classe DataTable (tabela de dados) 
+            //objeto da classe DataTable (tabela de dados - formato do C#) 
             DataTable dt = new DataTable();
             //tentar realizar os conmandos dentro do try
             try
@@ -61,12 +63,13 @@ namespace BancoDeTalentos
                 //Cria um escopo que ao final de sua execução libera
                 //recursos automaticamente através do método Dispose()
                 //cria um objeto do tipo comando
+                //ConexaoBanco().CreateCommand() = conexao.CreateCommand()
                 using (var cmd = ConexaoBanco().CreateCommand())
                 {
                     //preencher o comando com a string sql
                     cmd.CommandText = sql;
                     //criar um SQLiteDataAdapter usando o comando e a conexão
-                    //e salva em da
+                    //faz a pesquisa no banco de dados e salva em da
                     da = new SQLiteDataAdapter(cmd.CommandText, ConexaoBanco());
                     //converte de DataAdapter para DataTable e preenche o dt
                     da.Fill(dt);
@@ -82,6 +85,52 @@ namespace BancoDeTalentos
                 //se acontecer algum erro no try exibe a exceção
                 throw ex;
             }
+        }
+
+        //Funções do Form Currículo
+        /*
+        public static void inserirCurriculo(Curriculo c)
+        {
+            if (existeNome(c))
+            {
+                MessageBox.Show("Nome já cadastrado.");
+                return;
+            }
+            try
+            {
+                SQLiteDataAdapter da = null;
+                DataTable dt = new DataTable();
+                using (var cmd = ConexaoBanco().CreateCommand())
+                {
+                    //preencher o comando com a string sql
+                    cmd.CommandText = "INSERT INTO t_curriculo (nome, telefone, dataNascimento, escolaridade, profissao1, profissao2, curso1, curso2) VALUES (@nome, @telefone, @dataNascimento, @escolaridade, @profissao1, @profissao2, @curso1, @curso2)";
+                    //criar um SQLiteDataAdapter usando o comando e a conexão
+                    //e salva em da
+                    da = new SQLiteDataAdapter(cmd.CommandText, ConexaoBanco());
+                    //converte de DataAdapter para DataTable e preenche o dt
+                    da.Fill(dt);
+                    //fecha a conexão
+                    ConexaoBanco().Close();
+                    //retorna o DataTable dt
+                    return dt;
+                }
+            }
+        }
+        */
+        //Fim das funções Form Currículo
+
+        //ROTINAS GERAIS
+        public static bool existeNome(Curriculo c)
+        {
+            DataTable dt = null;
+            bool res = false;
+            string sql = "SELECT nome FROM t_curriculo WHERE nome = '" + c.nome + "'";
+            Banco.consulta(sql);
+            if (dt.Rows.Count > 0)
+            {
+                res = true;
+            }
+            return res;
         }
     }
 }
--- Forms/BancoDeTalentos/BancoDeTalentos/Form1.cs
+++ Forms/BancoDeTalentos/BancoDeTalentos/Form1.cs
@@ -12,6 +12,9 @@ namespace BancoDeTalentos
 {
     public partial class Form1 : Form
     {
+        //Cria objeto dt da classe DataTable
+        //Esse objeto servirá para armazenar as informações
+        //que virão do banco de dados
         DataTable dt = new DataTable();
         public Form1()
         {
@@ -28,30 +31,33 @@ namespace BancoDeTalentos
                 tbxUsuario.Focus();
                 return;
             }
-
+            //comando sql para selecionar todas as colunas da tabela usuario
+            //onde o usuario e a senha forem iguais as digitadas
+            //na tela de login
             string sql = "SELECT * FROM t_usuarios WHERE usuario = '" 
-                + usuario + "' AND senha = '" + senha+"'";
+                + usuario + "' AND senha = '" + senha +"'";
+            //IDA - chama o método consulta passando a string sql
+            //como parâmetro, esse é um método static que
+            //pertence a classe Banco
+            //VOLTA - retorna a tabela de dados do banco de dados
+            //e salva no dt
             dt = Banco.consulta(sql);
+            //Conta quantas linhas tem da tabela de dados
+            //e verifica se tem uma linha
             if(dt.Rows.Count == 1)
             {
+                //Cria um objeto do tipo Dashboard (form) e 
+                //passar o usuário como parâmetro
+                //dt - tabela de dados
+                //Rows[0] - primeira linha
+                //ItemArray[1] - segundo (item/coluna) da linha
                 Dashboard d = new Dashboard(dt.Rows[0].ItemArray[1].ToString());
                 //dt.Rows[0].Field<string>("usuario");
                 d.Show();
             }
-            /*
-            Usuario u1 = new Usuario();
-
-            if(usuario == u1.getUsuario() && senha == u1.getSenha()) 
-            {
-                Dashboard d1 = new Dashboard(usuario);
-                d1.ShowDialog();
-            }
-            else
-            {
-                lblMensagem.Text = "Usuário ou Senha incorreto";
-            }
-            */
         }
+
+        
 
         private void label3_Click(object sender, EventArgs e)
         {
